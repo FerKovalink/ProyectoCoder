93030ea7441a29a1c281814ce64e74487d68dd4d
Fer Kova <ferkovalink@gmail.com>
2022-04-01 13:09:59 -0300

    nueva carpeta, static

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,11 +3,35 @@ from django.http import HttpResponse
 from AppCoder.models import Curso
 
 def curso(self):
-    curso = Curso(nombre= 'Web', camada= '1111')
+    curso = Curso(nombre= 'Desarrollo Web', camada= '1111')
 
     curso.save()
 
     documento = f'El curso es: {curso.nombre}, la camada: {curso.camada}'
 
     return HttpResponse(documento)
+
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+
+def curos(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
+
+
+
+
+
+
+
+
+
 
